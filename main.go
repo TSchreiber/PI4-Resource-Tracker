c4624ca6fccf3af43f3def923fcb0175f2fa66cc
Daniel Schreiber <daniel.schreiber11@gmail.com>
2021-08-06 21:14:11 -0500

    fix: mpstat cpu usage values
    Fixed issue where mpstat was not returning the correct values. Previously mpstat was being called each time the screen was being updated, but that is not how mpstat is intended to be used. It was returning the cpu usage overall since (I think) the last boot. Now mpstat is being called from within a goroutine and it is correctly returning average cpu usage for each second of monitoring.

--- main.go
+++ main.go
@@ -7,34 +7,72 @@ import (
     "strconv"
     "strings"
     "time"
+    "bufio"
 )
+
+var cpuUsage [4]int
 
 func main() {
     // Clear the screen
     fmt.Print("\033[H\033[2J")
+    MonitorCpuUsage()
     for true {
         fmt.Print(getPrintBuffer())
         time.Sleep(1 * time.Second)
     }
 }
 
+/**
+ * Creates a mpstat process and updates the int array every time the cpu usage
+ * changes.
+ */
+func MonitorCpuUsage() [4]int {
+    // cmd := exec.Command("python","mpstat.py","-P","0-3","1")
+    cmd := exec.Command("mpstat","-P","0-3","1")
+    r, _ := cmd.StdoutPipe()
+    cmd.Stderr = cmd.Stdout
+    scanner := bufio.NewScanner(r)
+    go func() {
+        for scanner.Scan() {
+            line := scanner.Text()
+            fields := strings.Fields(line)
+            if len(fields) == 13 {
+                cpuNum, err := strconv.Atoi(fields[2])
+                if err == nil {
+                    fIdleCpu, err := strconv.ParseFloat(fields[12], 64)
+                    if err == nil {
+                        cpuUsage[cpuNum] = int((100 - fIdleCpu) * 100)
+                    }
+                }
+            }
+        }
+    }()
+    cmd.Start()
+    return cpuUsage
+}
+
+/**
+ * Calculates and compiles all the information that needs to go onto the console
+ * into a single string so it can be printed in one go. This limits tearing
+ * during redraws.
+ */
 func getPrintBuffer() string {
     var output string = "\033[H"
     output += getTempuratureString();
-    stdout, _ := exec.Command("mpstat", "-P", "0-3").Output()
-    // stdout, _ := exec.Command("python", "mpstat.py").Output()
-    for _, line := range strings.Split(string(stdout), "\n")[3:7] {
-        fields := strings.Fields(line)
-        cpuNum, idlePercent := fields[2], fields[12]
-        fIdlePercent, _ := strconv.ParseFloat(idlePercent, 64)
-        cpuPercent := int(100 - fIdlePercent)
-        output += fmt.Sprint(SprintProgressBar("CPU" + cpuNum, 70, cpuPercent))
+    for cpuNum, cpuPermyriad := range cpuUsage {
+        output += GetProgressBar("CPU" + strconv.Itoa(cpuNum), 70, cpuPermyriad / 100)
     }
     return output
 }
 
+/**
+ * returns the tempurature reading from the hardwear measurement device. For
+ * linux systems, this is located in the file,
+ *     "/sys/class/thermal/thermal_zone0/temp"
+ * For windows system, wmic needs to be used.
+ */
 func getTempuratureString() string {
-    //file, err := os.Open("sys.class.thermal.thermal_zone0.temp")
+    // file, err := os.Open("sys.class.thermal.thermal_zone0.temp")
     file, err := os.Open("/sys/class/thermal/thermal_zone0/temp")
     if err != nil {
         panic(err)
@@ -46,7 +84,7 @@ func getTempuratureString() string {
     return fmt.Sprintf("%.1f°C\n", float64(milicelcius) / 1000.0)
 }
 
-func SprintProgressBar(title string, width, percent int) string {
+func GetProgressBar(title string, width, percent int) string {
     var output string = ""
     fillWidth := width - 2
     numFilledBlocks := fillWidth * percent / 100
@@ -56,15 +94,15 @@ func SprintProgressBar(title string, width, percent int) string {
         strings.Repeat(" ", fillWidth / 2),
         strPercent,
         strings.Repeat(" ", fillWidth / 2 - len(strPercent)))
-    output += fmt.Sprint("\u2502")
-    output += fmt.Sprint("\033[48;2;141;150;83m")
+    output += "\u2502"
+    output += "\033[48;2;141;150;83m"
     for i, c := range centeredPercent {
         if i == numFilledBlocks {
-            output += fmt.Sprint("\033[49m")
+            output += "\033[49m"
         }
-        output += fmt.Sprint(string(c))
+        output += string(c)
     }
-    output += fmt.Sprintln("\033[49m\u2502")
+    output += "\033[49m\u2502\n"
     output += fmt.Sprintf("\u2514%s\u2518\n", strings.Repeat("\u2500",fillWidth))
     return output
 }
